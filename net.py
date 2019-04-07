106015592cbe16dd95238a35af0be074f36f5758
silviosantana <silviosantana28@hotmail.com>
2019-04-07 11:04:45 +0200

    deeper vgg

--- net.py
+++ net.py
@@ -33,6 +33,7 @@ decoder = nn.Sequential(
     nn.ReLU(),
     nn.ReflectionPad2d((1, 1, 1, 1)),
     nn.Conv2d(64, 3, (3, 3)),
+    nn.Upsample(scale_factor=2, mode='nearest'),
 )
 
 vgg = nn.Sequential(
@@ -100,25 +101,38 @@ class Net(nn.Module):
         self.enc_2 = nn.Sequential(*enc_layers[4:11])  # relu1_1 -> relu2_1
         self.enc_3 = nn.Sequential(*enc_layers[11:18])  # relu2_1 -> relu3_1
         self.enc_4 = nn.Sequential(*enc_layers[18:31])  # relu3_1 -> relu4_1
+
+        self.enc_5 = nn.Sequential(*enc_layers[31:40])  # relu4_1 -> relu5_1
+        self.enc_6 = nn.Sequential(*enc_layers[40:53])  # relu5_1 -> relu5_4
+
+
         self.decoder = decoder
+
+        self.num_enc = 6
+
         self.mse_loss = nn.MSELoss()
 
-        # fix the encoder
+        """         # fix the encoder
         for name in ['enc_1', 'enc_2', 'enc_3', 'enc_4']:
             for param in getattr(self, name).parameters():
+                param.requires_grad = False """
+
+        # fix the encoder
+        for i in range(self.num_enc):
+            for param in getattr(self, 'enc_{:d}'.format(i + 1)).parameters():
                 param.requires_grad = False
 
     # extract relu1_1, relu2_1, relu3_1, relu4_1 from input image
     def encode_with_intermediate(self, input):
         results = [input]
-        for i in range(4):
+        for i in range(self.num_enc):
             func = getattr(self, 'enc_{:d}'.format(i + 1))
             results.append(func(results[-1]))
         return results[1:]
 
     # extract relu4_1 from input image
     def encode(self, input):
-        for i in range(4):
+        for i in range(self.num_enc):
             input = getattr(self, 'enc_{:d}'.format(i + 1))(input)
         return input
 
@@ -144,9 +158,9 @@ class Net(nn.Module):
 
         g_t = self.decoder(t)
         g_t_feats = self.encode_with_intermediate(g_t)
-
+        #print(g_t_feats[-1].shape, t.shape)
         loss_c = self.calc_content_loss(g_t_feats[-1], t)
         loss_s = self.calc_style_loss(g_t_feats[0], style_feats[0])
-        for i in range(1, 4):
+        for i in range(1, len(style_feats)):
             loss_s += self.calc_style_loss(g_t_feats[i], style_feats[i])
         return loss_c, loss_s
