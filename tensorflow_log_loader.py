3a548da5953e5228a9897b9238faf0c36501d233
Kalus <xkrato39@stud.feec.vutbr.cz>
2019-04-25 20:47:26 +0200

    Add trend

--- tensorflow_log_loader.py
+++ tensorflow_log_loader.py
@@ -13,6 +13,8 @@ def plot_tensorflow_log(args):
     event_acc = EventAccumulator(args.log_name, tf_size_guidance)
     event_acc.Reload()
     
+    assert event_acc.Tags()["scalars"] != [], "Did you give the log file?"
+    
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(1, 1, 1)
 
@@ -22,33 +24,53 @@ def plot_tensorflow_log(args):
         loss = event_acc.Scalars('loss')
         
         steps = len(loss)
-        x = np.zeros([steps, 1])
-        y = np.zeros([steps, 1])
+        x = np.zeros(steps,)
+        y = np.zeros(steps,)
         for i in range(steps):
             x[i] = loss[i][1] #step
             y[i] = loss[i][2] # value
-        ax1.plot(x, y[:], label='loss_auto')
+        ax1.plot(x, y[:], label='loss_auto', color="tab:orange")
+        if args.trend:
+            if not args.linear:
+                y_r=np.log(y)
+                y_trend = np.exp(np.poly1d(np.polyfit(x,y_r[:,0],5))(x))
+            else:
+                y_trend = np.poly1d(np.polyfit(x,y[:,0],5))(x)
+
+            ax1.plot(x, y_trend, label='loss_trend', color="red")
     else:
         loss_content = event_acc.Scalars('loss_content')
         loss_style = event_acc.Scalars('loss_style')
 
         steps = len(loss_content)
-        x = np.zeros([steps, 1])#np.arange(steps)
-        y = np.zeros([steps, 2])
+        x = np.zeros(steps, )
+        y = np.zeros([steps,2])
 
         for i in range(steps):
             x[i] = loss_content[i][1] #step
-            y[i, 0] = loss_content[i][2] # value
-            y[i, 1] = loss_style[i][2]
+            y[i, 0] = loss_style[i][2]
+            y[i, 1] = loss_content[i][2] # value
 
-        ax1.plot(x, y[:,0], label='loss_content')
-        ax1.plot(x, y[:,1], label='loss_style')
+        ax1.plot(x, y[:,0], label='loss_style', color="tab:orange")
+        ax1.plot(x, y[:,1], label='loss_content', color="tab:blue")
+        if args.trend:
+            if not args.linear:
+                y_r=np.log(y)
+                y_trend1 = np.exp(np.poly1d(np.polyfit(x,y_r[:,0],5))(x))
+                y_trend2 = np.exp(np.poly1d(np.polyfit(x,y_r[:,1],5))(x))
+            else:
+                y_trend1 = np.poly1d(np.polyfit(x,y[:,0],5))(x)
+                y_trend2 = np.poly1d(np.polyfit(x,y[:,1],5))(x)
+
+            ax1.plot(x, y_trend1, label='loss_style_trend', color="red")
+            ax1.plot(x, y_trend2, label='loss_content_trend', color="blue")
 
     ax1.set_xlabel("Iter")
     ax1.set_ylabel("loss")
     ax1.set_title("Training Progress")
     ax1.legend(loc='upper right', frameon=True)
-    ax1.set_yscale("log")
+    if not args.linear:
+        ax1.set_yscale("log")
     if args.save:
         if not os.path.exists(args.save_dir):
             os.mkdir(args.save_dir)
@@ -59,12 +81,14 @@ def plot_tensorflow_log(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log_dir', type=str, default='./logs' , help='Log dir')
-    parser.add_argument('--log_name', type=str, default=None ,help='Log name')
+    parser.add_argument('--log_dir', type=str, default='./logs', help='Log dir')
+    parser.add_argument('--log_name', type=str, default=None, help='Log name')
     parser.add_argument('--save', action='store_true')
+    parser.add_argument('--linear', action='store_true', help='Linear scale [default: logaritmic]')
+    parser.add_argument('--trend', action='store_false', help='disable plotting trend')
     parser.add_argument('--save_dir', type=str, default='./plots',
 			help='Dir for save plot, default ./plots')
-    parser.add_argument('--num_load', type=int, default=160000)
+    parser.add_argument('--num_load', type=int, default=80000, help='Number of loaded data samples [default: 80000]')
     args = parser.parse_args()
     
     if args.log_name == None:
