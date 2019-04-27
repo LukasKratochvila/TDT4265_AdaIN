e36c59bd083f44046c9d64874f380a6acff442fd
Kalus <kratochvila.lukas@gmail.com>
2019-04-27 16:03:00 +0200

    Fix the computing losses on GPU

--- test.py
+++ test.py
@@ -95,7 +95,6 @@ network = net.Net(encoder, decoder)
 network.decoder.eval()
 for i in range(network.num_enc):
     getattr(network, 'enc_{:d}'.format(i + 1)).eval()
-    getattr(network, 'enc_{:d}'.format(i + 1)).to(device)
 
 network.to(device)
 
@@ -128,7 +127,7 @@ for i,content_path in enumerate(content_paths):
         for i, w in enumerate(interpolation_weights):
             style_c += w * style[i] 
         result = output[:style_c.shape[0],:style_c.shape[1],:style_c.shape[2],:style_c.shape[3]]
-        result.to(device)
+        result = result.to(device)
         content_c = content[:result.shape[0],:result.shape[1],:result.shape[2],:result.shape[3]]
         style_c = style[:result.shape[0],:result.shape[1],:result.shape[2],:result.shape[3]]
         content_losses[i] = network.losses(content_c,result).cpu()
@@ -154,7 +153,7 @@ for i,content_path in enumerate(content_paths):
                 save_image(output, output_name)
             
             result = output[:style.shape[0],:style.shape[1],:style.shape[2],:style.shape[3]]
-            result.to(device)
+            result = result.to(device)
             content_c = content[:result.shape[0],:result.shape[1],:result.shape[2],:result.shape[3]]
             style_c = style[:result.shape[0],:result.shape[1],:result.shape[2],:result.shape[3]]
             content_losses[i][j] = network.losses(content_c,result).cpu()
